79457fd7c71c8e47ceba88073347625de5602b07
Maximilian Kerp <mkerp2s@smail.inf.h-brs.de>
2014-06-21 20:37:31 +0200

    Erverything is working now as expected. Exercise done. :)

--- operating_systems/u11/msgctrl.c
+++ operating_systems/u11/msgctrl.c
@@ -28,6 +28,7 @@
 #include <sys/types.h> // msgrcv(), msgsnd()
 #include <sys/ipc.h>   //
 #include <sys/msg.h>   //
+#include <string.h>    // strcpy()
 
 //==============================================================================
 // DEFINES
@@ -40,8 +41,8 @@ int err;
 //==============================================================================
 // msg_t definition
 typedef struct {
-  long msg_type;      // Type of the message
-  char *msg;  // The message itself
+  long type;      // Type of the message
+  char text[MSG_LEN];  // The message itself
 } msg_t;
 
 //==============================================================================
@@ -86,7 +87,9 @@ int send_msg (int key, int type, char *message)
 {
   int msg_id;
 
-  msg_t msg = {.msg_type=type, .msg=message};
+  msg_t msg;
+  msg.type = type;
+  strcpy(msg.text, message);
 
   // Create or get the message queue and return the msg_id
   if( (msg_id = msgget(key, IPC_CREAT | 0666)) == -1) {
@@ -94,15 +97,14 @@ int send_msg (int key, int type, char *message)
     exit(EXIT_FAILURE);
   }
 
-  printf("msg_id: %d\n", msg_id);
-
   // Send message to the queue, wait if queue is full (NULL)
   if ( (err = msgsnd(msg_id, &msg, MSG_LEN, 0)) == -1 ) {
     perror("Error sending message to queue");
     exit(EXIT_FAILURE);
   }
 
-  printf("Message send successfully to queue with key: %d\n", key);
+  printf("Message send successfully to queue with key %d (id: %d)\n",
+      key, msg_id);
 
   return 0;
 }
@@ -127,8 +129,8 @@ int rcv_msg (int key, int type)
     exit(EXIT_FAILURE);
   }
 
-  printf("Message received successfully from queue with key: %d\n" \
-      "MESSAGE: %s", key, msg.msg);
+  printf("Message received successfully from queue with key %d (id: %d)\n" \
+      "MESSAGE: %s\n", key, msg_id, msg.text);
 
   return 0;
 }
